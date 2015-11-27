c9acf80553061bde6b376047ac21c8086f893837
Glenn Dierckx <glenn@windstyle.be>
2015-11-27 16:58:43 +0100

    Added project

---
+++ Cloudbrew/CloudBrew.IoT.WebJob/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices;
+using Microsoft.Azure.WebJobs;
+using Microsoft.ServiceBus.Messaging;
+
+namespace CloudBrew.IoT.WebJob
+{
+    // To learn more about Microsoft Azure WebJobs SDK, please see http://go.microsoft.com/fwlink/?LinkID=320976
+    public class Program
+    {
+        private static JobHost _host;
+        // Please set the following connection strings in app.config for this WebJob to run:
+        // AzureWebJobsDashboard and AzureWebJobsStorage
+        static void Main()
+        {
+            _host = new JobHost();
+            _host.RunAndBlock();
+        }
+    }
+}
