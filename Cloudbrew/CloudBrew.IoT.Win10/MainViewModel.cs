b3792262bc2e953a4966a4006693d58f710f1795
Glenn Dierckx <glenn@windstyle.be>
2015-11-27 16:37:42 +0100

    Added serial communication

--- Cloudbrew/CloudBrew.IoT.Win10/MainViewModel.cs
+++ Cloudbrew/CloudBrew.IoT.Win10/MainViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.ComponentModel;
+using System.Linq;
 using System.Runtime.CompilerServices;
+using System.Text;
 using Windows.UI.Core;
 using CloudBrew.IoT.Win10.Annotations;
 
@@ -10,12 +12,16 @@ namespace CloudBrew.IoT.Win10
     {
         private readonly CoreDispatcher _dispatcher;
         private string _deviceId;
+        private SerialCommunication _serialCommunication;
+        private StringBuilder _sbLog;
 
         public MainViewModel(CoreDispatcher dispatcher)
         {
             _dispatcher = dispatcher;
             DeviceId = "**Device id**";
             Init();
+            _sbLog = new StringBuilder();
+            WriteLog("Registered!");
         }
 
         private async void Init()
@@ -23,6 +29,16 @@ namespace CloudBrew.IoT.Win10
             var deviceRegistration = new DeviceRegistration();
             var device = await deviceRegistration.GetOrCreateDevice();
             DeviceId = device.Id.ToString();
+            var serialPorts = await SerialCommunication.ListAvailablePorts();
+            _serialCommunication = new SerialCommunication(serialPorts.First());
+            _serialCommunication.OnMessage += OnSerialMessage;
+        }
+
+        private void OnSerialMessage(object sender, MessageEventArgs e)
+        {
+            WriteLog(e.Message);
+            WriteLog("Sending message: 1000");
+            _serialCommunication.SendAsync("1000");
         }
 
         public string DeviceId
@@ -30,25 +46,32 @@ namespace CloudBrew.IoT.Win10
             get { return _deviceId; }
             set
             {
-                if (_dispatcher.HasThreadAccess)
-                {
-                    if (value == _deviceId) return;
-                    _deviceId = value;
-                    OnPropertyChanged();
-                }
-                else
-                {
-                    _dispatcher.RunAsync(CoreDispatcherPriority.Normal, () => DeviceId = value);
-                }
+                if (value == _deviceId) return;
+                _deviceId = value;
+                OnPropertyChanged();
             }
         }
 
+        public string Log => _sbLog.ToString();
+
+        public void WriteLog(string message)
+        {
+            _sbLog.AppendLine(message);
+            OnPropertyChanged("Log");
+        }
         public event PropertyChangedEventHandler PropertyChanged;
 
         [NotifyPropertyChangedInvocator]
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            if (_dispatcher.HasThreadAccess)
+            {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+            else
+            {
+                _dispatcher.RunAsync(CoreDispatcherPriority.Normal, () => OnPropertyChanged(propertyName));
+            }
         }
     }
 }
